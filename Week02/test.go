ba2f205c22f29642407ffa91dfd22fbc75022e9c
liuyongchen <414527086@qq.com>
2020-12-10 08:10:17 +0800

    config

--- Week02/test.go
+++ Week02/test.go
@@ -1,9 +1,82 @@
-package Week02
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"src/github.com/axgle/mahonia"
 )
 
-func tt() {
+var (
+	path = "D:/matt_liu/学习/go语言/2020.11.28/data/"
+	oFile = "kaifang.txt"
+	goodFile = "kaifang_good.txt"
+	badFile = "kaifang_bad.txt"
+)
+
+type Config struct {
+	Server  `config:"server"`
+	Mysql	`config:"mysql"`
+}
+
+type Server struct {
+	Ip string
+	Port int
+}
+
+type Mysql struct {
+	UserName string
+	PassWd string
+	DataBase string
+	Host string
+	Port int
+	TimeOut float64
+}
+
+func main() {
+
+
+}
+
+func cleanDatas()  {
+	oldFile, err := os.Open(path + oFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer oldFile.Close()
+
+	goodFile, err := os.OpenFile(path+goodFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer goodFile.Close()
+	badFile, err := os.OpenFile(path+badFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer badFile.Close()
+
+	myBytes := bufio.NewReader(oldFile)
+
+	for {
+		lineBytes, _, err := myBytes.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		lineStr := string(lineBytes)
+		enc := mahonia.NewDecoder("GBK")
+		convertStr := enc.ConvertString(lineStr)
+		fields := strings.Split(convertStr, ",")
+		if len(fields) >= 2 && len(fields[1]) == 18 {
+			goodFile.WriteString(convertStr+"\n")
+			fmt.Println("good:",convertStr)
+		}else {
+			badFile.WriteString(convertStr+"\n")
+			fmt.Println("bad", convertStr)
+		}
+	}
 
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module golearn/Go-000
+
+go 1.12
+
+require golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
